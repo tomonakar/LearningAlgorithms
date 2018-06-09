c27feddce43296a251576a763f94fa375e5d1c46
tomonakar <tomohisa.nak@gmail.com>
2018-06-09 16:41:14 +0900

    Logarithmic runtime. Big O Notation: O(log n)

--- harmlessRansomNote.js
+++ harmlessRansomNote.js
@@ -42,3 +42,26 @@ function addAndLog(array) {
 addAndLog(['A', 'B', 'C'])  // 9 pairs logged out
 addAndLog(['A', 'B', 'C', 'D']) //16 pairs logged out
 andAddLog(['A', 'B', 'C', 'D', 'E'])// 25 pairs logged out
+
+
+// Logaritmic runtime
+// Big O Notation: 'O(log n)'
+function binarySearch(array, key) {
+  var low = 0
+  var high = array.length -1
+  var mid
+  var element
+
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2, 10)
+    element = array[mid]
+    if (element < key) {
+      low = mid + 1
+    } else if (element > key) {
+      high = mid - 1
+    } else {
+      return mid
+    }
+  }
+  return -1
+}
