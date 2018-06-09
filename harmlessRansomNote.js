636e845834ecc1eec1fa82b6105eb84db9ef2a26
tomonakar <tomohisa.nak@gmail.com>
2018-06-09 16:21:34 +0900

    Linear runtime. Big O Notation: 'O(n)'

--- harmlessRansomNote.js
+++ harmlessRansomNote.js
@@ -4,7 +4,7 @@
 */
 
 // Constant runtime
-// Big O Notation: "0(1)"
+// Big O Notation: "O(1)"
 function log(array) {
   console.log(array[0])
   console.log(array[1])
@@ -12,3 +12,18 @@ function log(array) {
 
 log([1, 2, 3, 4])
 log([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+
+// Linear runtime
+// Big O Notatin: "O(n)"
+
+function logAll(array) {
+  for (var i = 0; i < array,length; i++) {
+    console.log(array[i]);
+  }
+}
+
+logAll([1, 2, 3, 4, 5])
+logAll([1, 2, 3, 4, 5, 6])
+logAll([1, 2, 3, 4, 5, 6, 7])
