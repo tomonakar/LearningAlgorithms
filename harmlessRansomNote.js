90a39a49a7f1e217bec07019a68aed9466c78052
tomonakar <tomohisa.nak@gmail.com>
2018-06-10 17:59:34 +0900

    feat: harmlessRansomeNote

--- harmlessRansomNote.js
+++ harmlessRansomNote.js
@@ -19,7 +19,7 @@ log([1, 2, 3, 4, 5, 6, 7, 8, 9])
 // Big O Notatin: "O(n)"
 
 function logAll(array) {
-  for (var i = 0; i < array,length; i++) {
+  for (var i = 0; i < array.length; i++) {
     console.log(array[i]);
   }
 }
@@ -32,7 +32,7 @@ logAll([1, 2, 3, 4, 5, 6, 7])
 // Exponential runtime
 // Big O Notation: 'O(n^2)'
 function addAndLog(array) {
-  for (var i = 0; i < array,length; i++) {
+  for (var i = 0; i < array.length; i++) {
     for (var j = 0; j < array.length; j++) {
       console.log(array[i] + array[j])
     }
@@ -41,7 +41,7 @@ function addAndLog(array) {
 
 addAndLog(['A', 'B', 'C'])  // 9 pairs logged out
 addAndLog(['A', 'B', 'C', 'D']) //16 pairs logged out
-andAddLog(['A', 'B', 'C', 'D', 'E'])// 25 pairs logged out
+addAndLog(['A', 'B', 'C', 'D', 'E'])// 25 pairs logged out
 
 
 // Logaritmic runtime
@@ -65,3 +65,29 @@ function binarySearch(array, key) {
   }
   return -1
 }
+
+
+// harmlessRansomeNote
+function harmlessRansomNote(noteText, magazineText) {
+  var noteArr = noteText.split(' ');
+  var magazineArr = magazineText.split(' ');
+  var magazineObj = {};
+
+  magazineArr.forEach(word => {
+    if (!magazineObj[word]) magazineObj[word] = 0;
+    magazineObj[word]++;
+  });
+
+  var noteIsPossible = true;
+  noteArr.forEach(word => {
+    if (magazineObj[word]) {
+      magazineObj[word]--;
+      if (magazineObj[word] < 0) noteIsPossible = false;
+    }
+    else noteIsPossible = false;
+  });
+
+  return noteIsPossible
+}
+
+harmlessRansomNote('this is a secret note for you from a secret admirer', 'puerto rico is a place of great wonder and excitement it has many secret waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited');
