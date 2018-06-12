3f8f5c2d1a15012c19d96f7c3da13e19894fffb5
tomonakar <tomohisa.nak@gmail.com>
2018-06-12 19:58:40 +0900

    refactor: 不要なコードの除去

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,4 @@
 function fizzBuzz(num) {
-  // let i = 0
-  // while(i <= num) {
-  //   console.log(i)
-  //   i++
-  // }
   for (var i = 1; i <= num; i++ ) {
     if (i % 3 === 0) console.log('Fizz')
     else if (i % 5 === 0) console.log('Buzz')
