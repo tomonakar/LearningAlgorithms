d46f53a80263e73c258ba9025ac5f17f97775f48
tomonakar <tomohisa.nak@gmail.com>
2018-06-14 10:36:55 +0900

    feat: 配列の中身を置き換えるアルゴリズムを基本的な形で実装

--- reverseArrayInPlace.js
+++ reverseArrayInPlace.js
@@ -1,8 +1,20 @@
 function reveseArrayInPlace(arr) {
   arr.forEach((item, index)=> {
-    arr.unshift(arr.splice(index,1)[0]
+    arr.unshift(arr.splice(index,1)[0])
   })
   return arr
 }
 
 console.log(reveseArrayInPlace([1,2,3,4,5]))
+
+
+function reveseArrayInPlaceBasic(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    var tempVar = arr[i]
+    arr[i] = arr[arr.length - 1 -i]
+    arr[arr.length -1 -i] = tempVar
+  }
+  return arr
+}
+
+console.log(reveseArrayInPlaceBasic([1,2,3,4,5]))
