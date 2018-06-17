e88fa2e813c8dc6ee3b89ec3f91c3af38850b953
tomonakar <tomohisa.nak@gmail.com>
2018-06-17 13:49:28 +0900

    fix: ポジション０の分岐を追加

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,7 @@
 function fibonacci(position) {
+  if (position === 0) return 0
   if (position < 3) return 1
   else return fibonacci(position -1) + fibonacci(position -2)
 }
 
-console.log('fibonacci(20)', fibonacci(20));
+console.log('fibonacci(20)', fibonacci(0));
